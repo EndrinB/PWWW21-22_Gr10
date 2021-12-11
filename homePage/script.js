251f002514ab112cfb2f46d8b812e8619351e008
EndrinB <endrin21@gmail.com>
2021-12-11 13:33:21 +0100

    validimi i fushave te login dhe sign up

--- homePage/script.js
+++ homePage/script.js
@@ -18,3 +18,43 @@ function closeSignUp() {
     document.getElementsByClassName('content')[0].style.filter = 'blur(0px)';
 
 }
+function validate(){
+  if(!(validateAge() && validatePasswords())){
+    return false;
+  }
+}
+function validatePasswords(){
+ 
+  let pw1 = document.getElementById("pw1").value;
+  let pw2 = document.getElementById("pw2").value;
+  
+  if(pw1 !== pw2){
+    alert("Passwords are different")
+    return false;
+    }
+  else{
+    return true;
+  }  
+}
+function validateAge(){
+  try{
+  let age = document.getElementById("age").value;
+  if(!isNumber(age)){
+    alert("Please Type a Number")
+    return false;
+  }
+  else if(Number.parseInt(age)<0){
+    alert("Age cant be lower then 0");
+    return false;
+  }
+  }
+  catch(e){
+    console.log(e);
+    throw new Error("Access Denied ");
+    
+  }
+  return true;
+}
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
