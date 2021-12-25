db7549ed831ee4139bb789bb9a73cc13f090e4a1
EndrinB <endrin21@gmail.com>
2021-12-25 23:33:11 +0100

    implementimi i drag and drop memory

--- uploadImagePage/uploadScript.js
+++ uploadImagePage/uploadScript.js
@@ -62,3 +62,69 @@ function drop(event) {
  //   event.target.insertAdjacentHTML("afterbegin", `<i class="fas fa-${draggableElementData}"></i>`);
   
 }
+
+// Upload your memory
+const dragArea = document.querySelector('.drag-area');
+const dragText = document.querySelector('.header');
+
+let button = document.querySelector('.button');
+let input = document.querySelector('.input');
+let file;
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+button.onclick = () =>{
+  input.click();
+};
+
+input.addEventListener('change', function() {
+  file = this.files[0];
+  dragArea.classList.add('active');
+     displayFile();
+});
+
+dragArea.addEventListener('dragover', (event) => {
+  event.preventDefault();
+  dragArea.classList.add('active');
+  dragText.textContent = 'Release to Upload';
+});
+
+dragArea.addEventListener('dragleave', (event) => {
+   dragText.textContent = 'Drag & Drop';
+   dragArea.classList.remove('active');
+});
+
+dragArea.addEventListener('drop', (event) => {
+  event.preventDefault();
+  
+   file = event.dataTransfer.files[0];
+   displayFile();
+});
+
+function displayFile(){
+  let fileType = file.type;
+
+  let validExtensions = ['image/jpeg','image/jpg','image/png'];
+
+  if(validExtensions.includes(fileType)){
+    let fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      let fileURL = fileReader.result;
+      let imgTag = `<img id="source" src="${fileURL}" alt="">`;
+      dragArea.innerHTML = imgTag;
+
+      let img = document.getElementById("source");
+
+      img.onload = () => {
+        ctx.drawImage(img,0,0,canvas.width,canvas.height);
+      };
+    };
+
+    fileReader.readAsDataURL(file);
+  }else{
+    alert('file not supported');
+    dragArea.classList.remove('active');
+  }
+};
